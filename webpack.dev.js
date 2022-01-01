29a413beb98cb0ce7b16b33ff77fc86f09314079
david okesanya <david.okesanya9@gmail.com>
2022-01-01 10:11:07 +0100

    renaming view folder to docs

--- webpack.dev.js
+++ webpack.dev.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/client/views/index.html',
+      template: './src/client/docs/index.html',
       filename: './index.html',
     }),
     new CleanWebpackPlugin({
